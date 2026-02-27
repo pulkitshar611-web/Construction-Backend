d2519dd231aebbe1f99566af96eeff46d74bb2d3
krishnarajput <161302830+KrishnpalJadam@users.noreply.github.com>
2026-02-27 14:48:25 +0530

    push

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const vendorController = require('../controllers/vendorController');
-const { protect } = require('../middlewares/authMiddleware');
+const { protect, authorize } = require('../middlewares/authMiddleware');
+
+// Public Routes (For External Trades)
+router.get('/public/drawing/:id', vendorController.getPublicDrawingInfo);
+router.post('/public/submit-bid', vendorController.submitBid);
 
 router.use(protect);
 
-router.post('/', vendorController.createVendor);
+// Vendor/Trade Management
+router.post('/', authorize('COMPANY_OWNER', 'PM'), vendorController.createVendor);
 router.get('/', vendorController.getVendors);
-router.delete('/:id', vendorController.deleteVendor);
+router.patch('/:id', authorize('COMPANY_OWNER', 'PM'), vendorController.updateVendor);
+router.delete('/:id', authorize('COMPANY_OWNER', 'PM'), vendorController.deleteVendor);
+
+// Drawing Distribution
+router.post('/send-drawing', authorize('COMPANY_OWNER', 'PM'), vendorController.sendDrawingToTrades);
+
+// Admin Bidding View
+router.get('/bids', authorize('COMPANY_OWNER', 'PM'), vendorController.getBids);
+router.patch('/bids/:id', authorize('COMPANY_OWNER', 'PM'), vendorController.updateBidStatus);
 
 module.exports = router;
